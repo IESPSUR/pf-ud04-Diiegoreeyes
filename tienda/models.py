2dfd5ef859bccc1ccf3a69c2fa4538d8b59d6a49
diiegoreeyes <diego.reyes.rosales.al@iespoligonosur.org>
2022-10-31 09:59:23 +0100

    BIEN

--- tienda/models.py
+++ tienda/models.py
@@ -1,3 +1,20 @@
+from django.conf import settings
 from django.db import models
-
+from django.utils import timezone
 # Create your models here.
+
+class producto(models.Model):
+    nombre = models.CharField(max_length=200)
+    modelo = models.CharField(max_length=200)
+    unidades = models.CharField(max_length=200)
+    precio = models.CharField(max_length=200)
+    detalles = models.TextField()
+
+
+class marca(models.Model):
+    marca = models.TextField()
+
+class compra(models.Model):
+    unidades = models.IntegerField
+    importe = models.IntegerField
+    fecha = models.DateTimeField(default=timezone.now)
