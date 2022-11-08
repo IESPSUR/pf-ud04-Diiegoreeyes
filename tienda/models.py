1dbc619e64b45ed02e5411b8c8257531c2f92f64
diiegoreeyes <diego.reyes.rosales.al@iespoligonosur.org>
2022-11-08 18:45:13 +0100

    FOREIG KEY

--- tienda/models.py
+++ tienda/models.py
@@ -2,16 +2,17 @@ from django.conf import settings
 from django.db import models
 from django.utils import timezone
 # Create your models here.
+class marca(models.Model):
+    nombre = models.CharField(max_length=200, primary_key='true')
 
 class producto(models.Model):
     nombre = models.CharField(max_length=200)
-    modelo = models.CharField(max_length=200)
+    modelo = models.CharField(max_length=200, primary_key='true')
     unidades = models.IntegerField()
     precio = models.IntegerField()
     detalles = models.CharField(max_length=200)
+    marca = models.ForeignKey(marca, on_delete=models.CASCADE)
 
-class marca(models.Model):
-    nombre = models.CharField(max_length=200)
 
 class compra(models.Model):
     unidades = models.CharField(max_length=200)
--- tienda/urls.py
+++ tienda/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path('', views.welcome, name='welcome'),
     path('tienda/', views.welcome, name='welcome'),
     path('tienda/informe/', views.informe, name='informe'),
+    path('tienda/actualizar/', views.informe, name='actualizar'),
 ]
