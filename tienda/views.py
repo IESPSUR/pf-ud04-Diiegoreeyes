6eb30c14fd0cea5699c631bace89bfa6dff697db
diiegoreeyes <diego.reyes.rosales.al@iespoligonosur.org>
2022-11-20 13:29:53 +0100

    FINISH

--- tienda/views.py
+++ tienda/views.py
@@ -8,7 +8,7 @@ from django.core.exceptions import ValidationError
 from django.db import transaction
 from datetime import datetime
 from django.shortcuts import render,redirect,get_object_or_404
-from django.db.models import Sum,Count
+from django.db.models import Sum
 
 from . import forms
 from .models import *
@@ -22,6 +22,7 @@ def welcome(request):
     return render(request,'tienda/index.html', {})
 
 # Vista Muestra todos los productos para CRUD #
+@staff_member_required
 def listado(request):
     listadoprod = producto.objects.all()
     return render(request, 'tienda/listado.html', {'listadoprod':listadoprod})
@@ -35,7 +36,7 @@ def listacompra(request):
 # Vista CREA USUARIO #
 def crearusuario(request):
     if request.method == 'POST':
-        # Usa Un Form propio de django de usuario
+        # Usamos Un Form propio de django para crear usuario
         form = UserCreationForm(request.POST)
         if form.is_valid():
             user = form.save()
@@ -47,11 +48,14 @@ def crearusuario(request):
 # Vista INICIAR SESION #
 def iniciar_sesion(request):
     if request.method == 'POST':
+        # Usamos Un Form propio de django para iniciar sesión
         form = AuthenticationForm(request, data=request.POST)
+        # Si es válido, recogemos los datos y se lo pasamos a user
         if form.is_valid():
             username = form.cleaned_data.get('username')
             password = form.cleaned_data.get('password')
             user = authenticate(username=username, password=password)
+            #Si existe, nos manda al welcome, sino manda un mensaje
             if user is not None:
                 login(request,user)
                 return redirect(welcome)
@@ -127,55 +131,71 @@ def eliminar(request, id):
     return redirect('listado')
 
 
+
+# INFORMES #
+@staff_member_required
+def informes(request):
+    return render(request, 'tienda/informes/informes.html', {})
+
+
+# Productos por marca #
 def listado_marca(request):
-
+    # Recogemos la marca por el formulario #
     marca = request.GET.get('marca')
-
+    # Si existe el check box del formulario #
     if marca:
-
+        # Recoge Marca del modelo  y filtramos por el valor del input#
         formulario = MarcaForm(request.GET)
         Productos = producto.objects.all().filter(marca=marca)
+        # Variable que usaremos #
         contexto = {'Productos':Productos, 'formulario':formulario}
-
+        # Si no existe el check box del formulario no le pasamos el producto #
     else:
         formulario = MarcaForm()
         contexto = {'formulario': formulario}
     return render(request, 'tienda/informes/listado_marca.html', contexto)
 
+# Listado de usuarios #
 def listado_usuario(request):
+
+    # Recogemos el usuario por el formulario #
 
     username = request.GET.get('user')
 
+    # Si existe el check box del formulario #
+
     if username:
+        # Recoge Usuario del modelo y filtramos por el valor del input#
 
         formulario = PersonaForm(request.GET)
         compras = compra.objects.all().filter(user=username)
         contexto = {'compras':compras, 'formulario':formulario}
 
     else:
+        # Si no existe el check box del formulario no le pasamos el usuario #
 
         formulario = PersonaForm()
         contexto = {'formulario': formulario}
 
     return render(request, 'tienda/informes/listado_usuario.html', contexto)
 
-
-def informes(request):
-    return render(request, 'tienda/informes/informes.html', {})
-
+#  TOP 10 PRODUCTOS #
 def toptenproductos(request):
-    unidadesvendidas = compra.objects.values('producto').annotate(total=Sum('importe'),sum_compras=Sum('unidades'),).order_by('-sum_compras')[:10]
+    # creamos variable, recogemos el producto del modelo compra y los ordena por la suma de compras #
+    unidadesvendidas = compra.objects.values('producto').annotate(total=Sum('importe'),sum_compras=Sum('unidades')).order_by('-sum_compras')[:10]
 
     Productos = producto.objects.all()
 
 
     return render(request, 'tienda/informes/toptenproductos.html', {'Productos':Productos, 'unidadesvendidas':unidadesvendidas})
 
+#  TOP 10 CLIENTES #
 
 def toptenclientes(request):
-    comprastotal = compra.objects.values('user').annotate(sum_compras=Sum('importe'))
-    Clientes = User.objects.annotate(importe_compras=Sum('compra__importe'),
-                                     total_compras=Count('compra')).order_by('-importe_compras')[:3]
+    # creamos variable, recogemos el usuario del modelo compra y los ordena por la suma de compras #
 
-    print(Clientes.query)
+    comprastotal = compra.objects.values('user').annotate(sum_compras=Sum('importe')).order_by('-sum_compras')[:10]
+
+    Clientes = User.objects.all()
+
     return render(request, 'tienda/informes/toptenclientes.html', {'Clientes': Clientes,'comprastotal':comprastotal})
